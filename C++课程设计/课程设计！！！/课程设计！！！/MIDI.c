6b301d1675a184b0d74fa91128220dd96bff273b
LinerZhang <Zhangliner0105@hotmail.com>
2025-03-27 11:32:53 +0800

    Delete C++课程设计/课程设计！！！/课程设计！！！/MIDI.c

--- C++课程设计/课程设计！！！/课程设计！！！/MIDI.c
+++
@@ -1,8 +0,0 @@
-#if 0
-#include<windows.h>
-//windows是用来使用system语句和插入图标，这里主要用于sleep(ms)，代表计算机暂停特定时间
-#include<mmsystem.h>//mmsystem是与多媒体有关的大多数接口
-#include<stdio.h>//stdio是输入与输出文件
-#include<conio.h>//conio是控制台字符输入输出操作
-#pragma comment(lib,"winmm.lib")
-#endif
